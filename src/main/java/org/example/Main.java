34c188148e8d016b27a357b14952d947e5c5f526
priyansh <raipriyansh20@gmail.com>
2025-03-30 04:35:04 +0530

    Test commit 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -53,7 +53,7 @@ public class Main {
             System.out.println("3. Natural Logarithm (ln(x))");
             System.out.println("4. Power Function (x^b)");
             System.out.println("5. Exit");
-            System.out.print("Enter your choice: ");
+            System.out.print(" Hey!! Enter your choice : ");
 
             try {
                 choice = Integer.parseInt(scanner.nextLine());
